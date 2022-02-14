c4dc3862d4d5185ad115b3ebdd5ff65090b004c4
Kataneki <99550200+kataneki@users.noreply.github.com>
2022-02-14 12:18:34 -0600

    Add files via upload

---
+++ c++_paint_calculator.cpp
@@ -0,0 +1,29 @@
+// Gavin Taylor
+// 2/14/2022
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	// 1 gallon paint = 340 sq ft
+	// fence = 600 sq ft
+	// 2 coats
+
+	// inputs
+	int numCoats = 2;
+	int numSqFence = 600;
+	int sqftGallon = 340;
+	double gallonsPaint = 0;
+
+
+	// calculations
+	double totalSqFoot = (numCoats * 1.0) * (numSqFence * 1.0);
+	gallonsPaint = totalSqFoot / sqftGallon;
+
+	// output
+	cout << "To paint " << numCoats << " coats on " << numSqFence << " square feet of fence, you will need " << gallonsPaint << " gallons of paint." << endl;
+
+	return 0;
+}
