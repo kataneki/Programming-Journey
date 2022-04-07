f6fb2e07cbe7802e1a3d12f34af11202fcf75f72
Kataneki <99550200+kataneki@users.noreply.github.com>
2022-04-07 15:54:08 -0500

    Update SkillPracticeAssignment6.cpp

--- SkillPracticeAssignment6.cpp
+++ SkillPracticeAssignment6.cpp
@@ -10,7 +10,6 @@ int main()
     double testScores[5];
 
     // Get inputs from user: student names, and student grades on assignments
-
     for (int i = 0; i < 5; i++) {
         cout << "What is the students name? ";
         getline(cin, getName[i]);
@@ -23,19 +22,30 @@ int main()
     }
 
     // Calculate student averages
-
     for (int k = 0; k < 5; k++) {
         testScores[k] = studentGrade[k][0] + studentGrade[k][1] + studentGrade[k][2] + studentGrade[k][3];
         testScores[k] = testScores[k] / 4;
     }
 
     // Print Student test scores to screen
-
-    cout << getName[0] << " has an average test score of: " << testScores[0] << endl;
-    cout << getName[1] << " has an average test score of: " << testScores[1] << endl;
-    cout << getName[2] << " has an average test score of: " << testScores[2] << endl;
-    cout << getName[3] << " has an average test score of: " << testScores[3] << endl;
-    cout << getName[4] << " has an average test score of: " << testScores[4] << endl;
+    for (int s = 0; s < 5; s++) {
+        cout << getName[s] << " has an average test score of: " << testScores[s];
+        if (testScores[s] >= 0 and testScores[s] < 60) {
+            cout << " - F" << endl;
+        }
+        if (testScores[s] >= 60 and testScores[s] < 70) {
+            cout << " - D" << endl;
+        }
+        if (testScores[s] >= 70 and testScores[s] < 80) {
+            cout << " - C" << endl;
+        }
+        if (testScores[s] >= 80 and testScores[s] < 90) {
+            cout << " - B" << endl;
+        }
+        if (testScores[s] >= 90 and testScores[s] <= 100) {
+            cout << " - A" << endl;
+        }
+    }
 
     return 0;
 }
