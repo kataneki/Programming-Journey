dd343a6379bf874723eddc138f1de3a9fbb58ce0
Kataneki <99550200+kataneki@users.noreply.github.com>
2022-04-23 13:56:54 -0500

    Update gradeOptions_FunctionsArrays.cpp

--- gradeOptions_FunctionsArrays.cpp
+++ gradeOptions_FunctionsArrays.cpp
@@ -1,10 +1,3 @@
-/******************************
-   Name
-   Date
-   PA3_Starter.cpp
-   Write a description of the program
-********************************/
-
 #include <iostream> // cout, cin
 #include <cstdlib> // exit()
 #include <string> // strings
@@ -23,7 +16,6 @@ void displayAverages(string students[], int grades[][MAX_GRADES], int studentCou
 void displayMax(string students[], int grades[][MAX_GRADES], int studentCount);
 void displayMin(string students[], int grades[][MAX_GRADES], int studentCount);
 char getLetterGrade(double grade);
-int getLongestNameLength(string students[], int studentCount);
 
 int main()
 {
@@ -68,26 +60,9 @@ int main()
 }
 
 /***********************************************************
-loadStudentNameGrades opens and read fileName. It will read in two strings,
-concatenate them, and then save
-to the students array. It then reads five integers and save each to the grades
-array. The function will return
-the actual number of student/grade combinations read
-PARAM: students is an array of strings that can hold up ot maxStudents values
-grades is a two dimensional array for holding the grades of each
-student
-fileName is the name of the file that will be opened and read
-maxStudents is the maximum number of students that we will read from
-the file
-PRE: students[] is large enough to contain up to maxStudents elements
-grades[] is large enough ot contain up to maxStudents elements
-POST: students[] contains the names of up to maxStudents
-grades[][] contains the grades for up to maxStudents
-The number of student/grade combinations actually read from the file is
-returned. This value can range
-between 0 <= numStudents <= maxStudents
-NOTE: students[] and grades[] are meant to be parralel arrays. students[0] and
-grades[0] are the same student
+loadStudentNameGrades opens and read fileName. It will read in two strings, concatenate them, 
+and then save to the students array. It then reads five integers and save each to the grades
+array. The function will return the actual number of student/grade combinations read
 ************************************************************/
 int loadStudentNamesGrades(string students[], int grades[][MAX_GRADES], string fileName, int maxStudents)
 {
@@ -113,20 +88,10 @@ int loadStudentNamesGrades(string students[], int grades[][MAX_GRADES], string f
 /***********************************************************
 displayAverages calculates the average of each student and displays the
 students name, average, and letter grade of the average in a table
-PARAM: students[] is an array of strings that contains the names of
-studentCount students
-grades[] is an array of integers that contains the grades of
-studentCount students
-studentCount contains the value of the number of elements in the
-students[] and grades[] arrays
-PRE: students[] and grades[] contain values for studentCount elements
-POST: table of student names, averages, and letter grades is displayed
 ************************************************************/
 void displayAverages(string students[], int grades[][MAX_GRADES], int studentCount)
 {
-    int gradeAverage[25]; 
-
-    cout << studentCount;
+    int gradeAverage[25];
 
     for (int i = 0; i < studentCount; i++) {
         gradeAverage[i] = grades[i][0] + grades[i][1] + grades[i][2] + grades[i][3] + grades[i][4];
@@ -134,12 +99,10 @@ void displayAverages(string students[], int grades[][MAX_GRADES], int studentCou
     }
 
     cout << "Average Grades" << endl;
-    cout << "Name ";
-    cout << setw(20) << right << "Average ";
-    cout << setw(20) << right << "Grade" << endl;
+    cout << setw(17) << left << "Name" << setw(8) << right << "Avg" << setw(8) << right << "Grade" << endl;
 
     for (int u = 0; u < studentCount; u++) {
-        cout << students[u] << " " << gradeAverage[u] << "CALL LETTER GRADE FUNCTION HERE" << endl;
+        cout << setw(17) << left << students[u] << setw(8) << right << gradeAverage[u] << setw(8) << getLetterGrade(gradeAverage[u]) << endl;
     }
     cout << endl;
 }
@@ -147,14 +110,6 @@ void displayAverages(string students[], int grades[][MAX_GRADES], int studentCou
 /***********************************************************
 displayMax calculates the maximum grade of each student and displays the
 students name, maximum grade, and letter grade of the maximum grade in a table
-PARAM: students[] is an array of strings that contains the names of
-studentCount students
-grades[] is an array of integers that contains the grades of
-studentCount students
-studentCount contains the value of the number of elements in the
-students[] and grades[] arrays
-PRE: students[] and grades[] contain values for studentCount elements
-POST: table of student names, maximum grades, and letter grades is displayed
 ************************************************************/
 void displayMax(string students[], int grades[][MAX_GRADES], int studentCount)
 {
@@ -170,10 +125,10 @@ void displayMax(string students[], int grades[][MAX_GRADES], int studentCount)
     }
 
     cout << "Max Grades" << endl;
-    cout << "Name" << "Max" << "Grade" << endl;
+    cout << setw(17) << left << "Name" << setw(8) << right << "Max" << setw(8) << right << "Grade" << endl;
 
     for (int u = 0; u < studentCount; u++) {
-        cout << students[u] << " " << studentGrade[u] << " " << getLetterGrade(studentGrade[u]) << endl;
+        cout << setw(17) << left << students[u] << setw(8) << right << studentGrade[u] << setw(8) << getLetterGrade(studentGrade[u]) << endl;
     }
     cout << endl;
 }
@@ -181,14 +136,6 @@ void displayMax(string students[], int grades[][MAX_GRADES], int studentCount)
 /***********************************************************
 displayMin calculates the minimum grade of each student and displays the
 students name, minimum grade, and letter grade of the minimum grade in a table
-PARAM: students[] is an array of strings that contains the names of
-studentCount students
-grades[] is an array of integers that contains the grades of
-studentCount students
-studentCount contains the value of the number of elements in the
-students[] and grades[] arrays
-PRE: students[] and grades[] contain values for studentCount elements
-POST: table of student names, minimum grades, and letter grades is displayed
 ************************************************************/
 void displayMin(string students[], int grades[][MAX_GRADES], int studentCount)
 {
@@ -204,51 +151,32 @@ void displayMin(string students[], int grades[][MAX_GRADES], int studentCount)
     }
 
     cout << "Min Grades" << endl;
-    cout << "Name          " << "Min         " << "Grade" << endl;
+    cout << setw(17) << left << "Name" << setw(8) << right << "Min" << setw(8) << right << "Grade" << endl;
 
     for (int u = 0; u < studentCount; u++) {
-        cout << setw(17) << left << students[u] << setw(8) << right << grade[u] << setw(5) << getLetterGrade(grade[u]) << endl;
+        cout << setw(17) << left << students[u] << setw(8) << right << grade[u] << setw(8) << getLetterGrade(grade[u]) << endl;
     }
     cout << endl;
 }
 
 /***********************************************************
 getLetterGrade converts a numerical grade to a letter grade
-PARAM: grade is the numerical grade to convert. Expected range is 0 <= grade
-<= 100
-PRE: grade contains a value in the correct range
-POST: The corresponding letter grade of the numerical grade is returned
 ************************************************************/
 char getLetterGrade(double grade)
 {
-        if (grade >= 0 and grade < 60) {
-            return 'F';
-        }
-        else if (grade >= 60 and grade < 70) {
-            return 'D';
-        }
-        else if (grade >= 70 and grade < 80) {
-            return 'C';
-        }
-        else if (grade >= 80 and grade < 90) {
-            return 'B';
-        }
-        else {
-            return 'A';
-        }
+    if (grade >= 0 and grade < 60) {
+        return 'F';
+    }
+    else if (grade >= 60 and grade < 70) {
+        return 'D';
+    }
+    else if (grade >= 70 and grade < 80) {
+        return 'C';
+    }
+    else if (grade >= 80 and grade < 90) {
+        return 'B';
+    }
+    else {
+        return 'A';
+    }
 }
-
-/***********************************************************
-getLongestNameLength returns the length of the longest string from a list of
-strings
-PARAM: students[] is an array of strings that contains the name of students
-studentCount is the size of the students[] array
-PRE: students[] contains studentCount names
-POST: The length of the longest string in students[] is returned
-************************************************************/
-/******************************
-int getLongestNameLength(string students[], int studentCount)
-{
-    
-}
-*******************************/
