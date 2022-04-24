7fc3bdc2be9bf624140ec9ce7eaf4c305b0147b5
Kataneki <99550200+kataneki@users.noreply.github.com>
2022-04-23 21:37:00 -0500

    Add files via upload

---
+++ SkillPracticeAssignment7.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+
+bool linearSearch(int, int[], int);
+bool binarySearch(int, int[], int);
+
+int main()
+{
+    const int valueSize = 20;
+    int intValue[valueSize] = {101, 142, 147, 189, 199, 207, 222, 234, 289, 296, 310, 319, 388, 394, 417, 429, 447, 521, 536, 600};
+    int correctValue;
+    int linearNum = 0;
+    int binaryNum = 0;
+
+    cout << "Please enter the Value you wish to search for: ";
+    cin >> correctValue;
+    cout << endl;
+
+    if (linearSearch(valueSize, intValue, correctValue))
+    {
+        cout << "You have found the value you were looking for with a linear search.\n";
+    }
+    else
+    {
+        cout << "The total number of linear searches was: " << valueSize << ".\n";
+        cout << "Your value was not found with a linear search.\n";
+    }
+    cout << endl;
+
+    if (binarySearch(valueSize, intValue, correctValue))
+    {
+        cout << "You have found the value you were looking for with a binary search.\n";
+    }
+    else
+    {
+        cout << "Your value was not found with a binary search.\n";
+    }
+
+    return 0;
+}
+
+bool linearSearch(int size, int list[], int value)
+{
+    bool found = false;
+    for (int count = 0; count < size && !found; count++)
+    {
+        if (list[count] == value) {
+            found = true;
+            cout << "The total number of linear searches were: " << count << ".\n";
+        }
+    }
+    return found;
+}
+
+bool binarySearch(int size, int list[], int value)
+{
+    bool found = false;
+    int first = 0;
+    int middle;
+    int last = size - 1;
+    int count = 0;
+
+    while (first <= last && !found)
+    {
+        middle = (first + last) / 2;
+
+        if (list[middle] == value)
+        {
+            found = true;
+            count++;
+            cout << "The total number of binary searches were: " << count << ".\n";
+        }
+        else if (list[middle] > value)
+        {
+            last = middle - 1;
+            count++;
+        }
+        else
+        {
+            first = middle + 1;
+            count++;
+        }
+    }
+    
+    if (value = !found)
+    {
+        cout << "The total number of binary searches were: " << count << ".\n";
+    }
+    
+    return found;
+}
