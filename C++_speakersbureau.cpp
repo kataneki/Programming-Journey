fa34a133113ce110bbe36c04fc8521097d387c1a
Kataneki <99550200+kataneki@users.noreply.github.com>
2022-04-28 15:23:11 -0500

    Update C++_speakersbureau.cpp

--- C++_speakersbureau.cpp
+++ C++_speakersbureau.cpp
@@ -3,15 +3,19 @@
 #include <iomanip>
 using namespace std;
 
-void speakerInfo(string speakerName, string speakerNumber, string speakerTopic, int speakerFee);
-void listData(string speakerName, string speakerNumber, string speakerTopic, int speakerFee);
-
-int main()
-{
+struct Speaker {
     string speakerName;
     string speakerNumber;
     string speakerTopic;
-    string speakerFee;
+    double speakerFee = 0.0;
+};
+
+void speakerInfo(Speaker speakers[]);
+void listData(Speaker speakers[]);
+
+int main()
+{
+    Speaker speakers[5];
     int userInput = 0;
 
     while (userInput != 3)
@@ -25,42 +29,47 @@ int main()
 
         if (userInput == 1)
         {
-            speakerInfo(speakerName, speakerNumber, speakerTopic, speakerFee);
+            speakerInfo(speakers);
         }
 
         else if (userInput == 2)
         {
-            listData(speakerName, speakerNumber, speakerTopic, speakerFee);
+            listData(speakers);
         }
-
     }
-
     return 0;
 }
 
-void speakerInfo(string speakerName, string speakerNumber, string speakerTopic, int speakerFee)
+void speakerInfo(Speaker speakers[])
 {
     string firstName;
     string lastName;
 
-    cout << "\nEnter the speakers' name: ";
-    cin >> firstName >> lastName;
-    speakerName = firstName + " " + lastName;
+    for (int a = 0; a < 5; a++)
+    {
+        cout << "\nEnter the speakers' name: ";
+        cin >> firstName >> lastName;
+        speakers[a].speakerName = firstName + " " + lastName;
 
-    cout << "Enter the speakers' telephone number: ";
-    cin >> speakerNumber;
+        cout << "Enter the speakers' telephone number: ";
+        cin >> speakers[a].speakerNumber;
 
-    cout << "Enter the speakers' speaking topic: ";
-    getline(cin, speakerTopic);
-    
-    cout << "Enter the speakers' required fee: ";
-    cin >> speakerFee;
+        cin.ignore();
+        cout << "Enter the speakers' speaking topic: ";
+        getline(cin, speakers[a].speakerTopic);
+
+        cout << "Enter the speakers' required fee: ";
+        cin >> speakers[a].speakerFee;
+    }
 }
 
-void listData(string speakerName, string speakerNumber, string speakerTopic, int speakerFee)
+void listData(Speaker speakers[])
 {
-    cout << "\nName: " << speakerName << endl << endl;
-    cout << "Telephone Number: " << speakerNumber << endl << endl;
-    cout << "Speaking Topic: " << speakerTopic << endl << endl;
-    cout << "Fee Required: " << speakerFee << endl << endl;
+    for (int b = 0; b < 5; b++)
+    {
+        cout << "\nName: " << speakers[b].speakerName << endl;
+        cout << "Telephone Number: " << speakers[b].speakerNumber << endl;
+        cout << "Speaking Topic: " << speakers[b].speakerTopic << endl;
+        cout << "Fee Required: " << speakers[b].speakerFee << endl << endl;
+    }
 }
